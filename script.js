78ed43139c5bcf550f3b371e1840348dba115249
McKinley Faustin <101219073+u276875@users.noreply.github.com>
2022-05-23 18:45:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("testing")
-
+  
+  return "I love you"
 } 
 
 // Write password to the #password input
