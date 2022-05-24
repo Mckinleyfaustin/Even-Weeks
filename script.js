6423f0b5c5b2e2b07bf182f27b00b7c6df39a2a2
McKinley Faustin <101219073+u276875@users.noreply.github.com>
2022-05-24 01:52:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-let arr = []
+//let arr = []
 
      let numbers = [0,1,2,3,4,5,6,7,8,9];
      let lowercase = "abcdefghijklmnopqrstuvwxyz";
