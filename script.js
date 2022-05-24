ea7b65e14336e68262a2e3cdd5334b86743a0560
McKinley Faustin <101219073+u276875@users.noreply.github.com>
2022-05-24 01:41:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 
+
 let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 let arr = []
 
      let numbers = [0,1,2,3,4,5,6,7,8,9];
-     let lowercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-     let uppercase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+     let lowercase = "abcdefghijklmnopqrstuvwxyz";
+     let uppercase = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
      let specialcharacters = ['@,#,$,%'];
      
-     let allCharacters = [...numbers, ...lowercase, ...uppercase, ...specialCharacters]
+     // let allCharacters = [...numbers, ...lowercase, ...uppercase, ...specialCharacters]
 
-// console.log(allDigits.length)
-
-// for(let i = 0; i < allCharacters; i++){
-//   Math.floor(Math.random() * allCharacters.length)
-// }
 
 function generatePassword() {
   var passwordLength = window.prompt("Choose a password length between 8 & 128")
@@ -72,16 +68,8 @@ function generatePassword() {
   const allCharacters = [];
   for (let i = 0; i < passwordLength; i++) {
     const characterCode = charCode[Math.floor(Math.random() * charCode.length)];
-    passwordCharacters.push(String.fromCharCode(characterCode));
-    console.log(characterCode);
+    //passwordCharacters.push(String.fromCharCode(characterCode));
+    //console.log(characterCode);
   }
-  return allCharacters.passwordlength.join('');
+  return ('');
 } 
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
